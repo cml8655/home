ce84898aae51dfeb7598a57d69109a0ec0da4334
menglin.chen <teamlab@192.168.1.131>
2015-05-25 16:09:24 +0800

    update

---
+++ Home/src/com/cml/product/home/activity/HomeActivity2.java
@@ -0,0 +1,20 @@
+package com.cml.product.home.activity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+import com.cml.product.home.R;
+import com.cml.product.home.ui.PushShowView;
+
+public class HomeActivity2 extends BaseActivity {
+	private LinearLayout container;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main2);
+
+
+	}
+}
