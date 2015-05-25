12caa25c3f236314b422475a1bcf51192f2ce9d9
menglin.chen <teamlab@192.168.1.131>
2015-05-25 17:19:05 +0800

    update

--- Home/src/com/cml/product/home/activity/HomeActivity.java
+++ Home/src/com/cml/product/home/activity/HomeActivity.java
@@ -13,6 +13,7 @@ public class HomeActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
 
 		container = (LinearLayout) findViewById(R.id.container);
--- Home/src/com/cml/product/home/activity/HomeActivity2.java
+++ Home/src/com/cml/product/home/activity/HomeActivity2.java
@@ -1,20 +1,55 @@
 package com.cml.product.home.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.Toast;
 
 import com.cml.product.home.R;
-import com.cml.product.home.ui.PushShowView;
 
 public class HomeActivity2 extends BaseActivity {
-	private LinearLayout container;
+
+	private LinearLayout header;
+	private LinearLayout item;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main2);
 
+		header = (LinearLayout) findViewById(R.id.layout_header);
+		item = (LinearLayout) findViewById(R.id.category_item);
 
+		OnItemClickListener listener = new OnItemClickListener();
+
+		item.setOnClickListener(listener);
+		header.setOnClickListener(listener);
+	}
+
+	private class OnItemClickListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+
+			Toast.makeText(
+					getApplicationContext(),
+					(v.getId() == R.id.layout_header) + ",,"
+							+ (v.getId() == R.id.category_item),
+					Toast.LENGTH_LONG).show();
+
+			Log.d("dddd", (v.getId() == R.id.layout_header) + ",,"
+					+ (v.getId() == R.id.category_item));
+
+			// titleµã»÷
+			if (v.getId() == R.id.layout_header) {
+				header.setVisibility(View.GONE);
+				item.setVisibility(View.VISIBLE);
+			} else {
+				header.setVisibility(View.VISIBLE);
+				item.setVisibility(View.GONE);
+			}
+		}
 	}
 }
