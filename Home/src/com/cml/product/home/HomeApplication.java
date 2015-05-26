8f314c7283f28e579758d5f10f3d52b830a1764c
menglin.chen <teamlab@192.168.1.131>
2015-05-26 15:30:16 +0800

    update

---
+++ Home/src/com/cml/product/home/HomeApplication.java
@@ -0,0 +1,10 @@
+package com.cml.product.home;
+
+import android.app.Application;
+
+public class HomeApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
