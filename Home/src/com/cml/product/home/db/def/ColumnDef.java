fe7f024b604aeebb75243003e89aa6d80e488cc7
menglin.chen <teamlab@192.168.1.131>
2015-05-27 18:01:44 +0800

    app表定义

---
+++ Home/src/com/cml/product/home/db/def/ColumnDef.java
@@ -0,0 +1,28 @@
+package com.cml.product.home.db.def;
+
+import android.provider.BaseColumns;
+
+public interface ColumnDef {
+	interface Tables {
+		String CATEGORY = "t_category";
+	}
+
+	interface Category extends BaseColumns {
+		String NAME = "category_name";
+		String TYPE = "category_type";
+		String ORDER = "category_order";
+		/** 分组图片，没有的话就是用默认 */
+		String ICON = "category_icon";
+	}
+
+	interface App extends BaseColumns {
+		String NAME = "app_name";
+		String PACKAGE = "app_package";
+		String CATEGORY = "category_id";
+		String ICON = "app_icon";
+		String ORDER = "app_order";
+		/** 1 系统应用 ,0 普通应用 */
+		String APP_FLG = "app_flg";
+		String START_TIMES="start_times";
+	}
+}
