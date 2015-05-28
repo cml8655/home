f563c689b90323a13807c95b6271c4639737da36
menglin.chen <teamlab@192.168.1.131>
2015-05-28 14:45:54 +0800

    update

--- Home/src/com/cml/product/home/fragment/dialog/LoadingFragment.java
+++ Home/src/com/cml/product/home/fragment/dialog/LoadingFragment.java
@@ -1,20 +1,19 @@
 package com.cml.product.home.fragment.dialog;
 
-import com.cml.product.home.R;
-
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
+import com.cml.product.home.R;
+
 public class LoadingFragment extends DialogFragment {
 
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-		LayoutInflater inflator=LayoutInflater.from(getActivity());
-		builder.setView(inflator.inflate(R.layout.dialog_loading, null));
-		return builder.create();
+		Dialog dialog = new Dialog(getActivity(), R.style.loadingStyle);
+		LayoutInflater inflator = LayoutInflater.from(getActivity());
+		dialog.setContentView(inflator.inflate(R.layout.dialog_loading, null));
+		return dialog;
 	}
 }
