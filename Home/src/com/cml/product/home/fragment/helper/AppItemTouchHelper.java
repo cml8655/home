334ceec3fe3b307b438cb9ee0c72fe19f2eeb26d
menglin.chen <teamlab@192.168.1.131>
2015-06-04 17:25:07 +0800

    update

--- Home/src/com/cml/product/home/fragment/helper/AppItemTouchHelper.java
+++ Home/src/com/cml/product/home/fragment/helper/AppItemTouchHelper.java
@@ -78,7 +78,7 @@ public class AppItemTouchHelper implements CategoryItemView.OnItemTouchListener 
 
 			@Override
 			public void onClick(DialogInterface dialog, int which) {
-				Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+				Intent intent = new Intent(Intent.ACTION_DELETE);
 				intent.setData(Uri.parse("package:" + pgName));
 				context.startActivity(intent);
 			}
